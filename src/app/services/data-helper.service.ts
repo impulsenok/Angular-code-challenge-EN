34db7530f81b2b6f6b7c566e871ff928c4cf4d64
Vitali <impulsenok@mail.ru>
2023-10-21 18:59:59 +0200

    feat(app): main functionality implementation.
    components structure;
    form implementation;
    simple form validation;
    custom validator for planeNumber;
    image component;
    directive to validate plateNumber;

---
+++ src/app/services/data-helper.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+import { Vehicles } from '../models/vehicle-types.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DataHelperService {
+
+  constructor() { }
+
+  getData(): Vehicles {
+    return {
+      auto: [
+        'Hatchback',
+        'Sedan',
+        'Station',
+        'Cabriolet',
+        'Coupé',
+        'Multi Purpose Vehicle (MVP)',
+        'Terreinauto',
+      ],
+      motor: [
+        'All-road',
+        'Naked',
+        'Enduro',
+        'Race',
+        'Toermotor',
+        'Chopper',
+        'Zijspan',
+      ],
+      scooter: []
+    }
+  }
+}
